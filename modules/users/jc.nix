18b529c7c73c7ca35d4b05301b507e4b1f989b13
JakeClark <jakeclark38b@gmail.com>
2025-08-11 23:26:35 +0700

    added streaming media packages

--- modules/users/jc.nix
+++ modules/users/jc.nix
@@ -7,15 +7,19 @@
     # Add common groups: network, sudo, docker
     extraGroups = [ "networkmanager" "wheel" "docker" ];
     packages = with pkgs; [
-      onlyoffice-desktopeditors # for office work
       vlc
+      gthumb
+      onlyoffice-desktopeditors # for office work
       nodejs # for gemini cli and more
       vscode
       docker
       docker-compose
-      python313Packages.uv
-      flameshot # for screenshot
       distrobox # for sandboxing applications
+      python313Packages.uv # for virtual environments
+      flameshot # for screenshot
+
+      ani-cli # for anime streaming
+      python313Packages.yt-dlp
     ];
   };
 }
