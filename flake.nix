d1cda06d972e57090bffb4ecc4c227714b6fa02e
JakeClark <jakeclark38b@gmail.com>
2025-08-12 14:27:09 +0700

    added nixos unstable with lmstudio

--- flake.nix
+++ flake.nix
@@ -8,6 +8,8 @@
   inputs = {
     # NixOS official package source, here using the nixos-25.05 branch
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    # Unstable for selected packages
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     # ---Snip---
     auto-cpufreq = {
         url = "github:AdnanHodzic/auto-cpufreq";
@@ -16,12 +18,29 @@
     # ---Snip---
   };
 
-  outputs = { self, nixpkgs, auto-cpufreq, ... }@inputs: {
+  outputs = { self, nixpkgs, nixpkgs-unstable, auto-cpufreq, ... }@inputs: {
     # The host with the hostname `JakeClark-Sep21st` will use this configuration
     nixosConfigurations."JakeClark-Sep21st" = nixpkgs.lib.nixosSystem {
       specialArgs = { inherit inputs; };
       system = "x86_64-linux";
       modules = [
+        # Overlay to get vscode from unstable
+        ({ pkgs, ... }: {
+          nixpkgs.overlays = [
+            (final: prev:
+              let
+                pkgsUnstable = import nixpkgs-unstable {
+                  system = final.stdenv.hostPlatform.system;
+                  config = final.config; # respects allowUnfree, etc.
+                };
+              in {
+                lmstudio = pkgsUnstable.lmstudio;
+                # If you use vscode-with-extensions, you can also map it here.
+                # vscode-with-extensions = pkgsUnstable.vscode-with-extensions;
+              })
+          ];
+        })
+
         ./configuration.nix
         auto-cpufreq.nixosModules.default
       ];
