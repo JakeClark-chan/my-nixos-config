4fad7546f6bed63a893ab5890fb7d85844fa93d8
JakeClark <jakeclark38b@gmail.com>
2025-08-18 13:46:32 +0700

    changed swap size and plymouth syntax

--- modules/system/memory.nix
+++ modules/system/memory.nix
@@ -4,14 +4,14 @@
   # Swap configuration
   swapDevices = [{
   device = "/swapfile";   # Path where swapfile will be created
-  size = 8 * 1024; # Size in MiB (4GB)
+  size = 6 * 1024; # Size in MiB (6GB)
   priority = 10;   # Priority of the swap device
   }];
 
   # Zram Swap
   zramSwap = {
   enable = true;        # Enable zram swap
-  memoryPercent = 33;
+  memoryPercent = 40;
   algorithm = "lz4";   # Compression algorithm to use
   priority = 100;       # Priority of the zram swap device
   };
--- modules/system/plymouth.nix
+++ modules/system/plymouth.nix
@@ -1,31 +1,21 @@
 { config, pkgs, ... }:
 
 {
-  # Enable Plymouth boot splash
-  boot.plymouth.enable = true;
-  
-  # Disable Plymouth password prompts during boot
-  boot.initrd.systemd.emergencyAccess = false;
-
-  boot.plymouth.theme = "breeze";
-  
-  # # Add kernel parameters to slow down boot for testing
-  # boot.kernelParams = [
-  #   "quiet"          # Hide kernel messages
-  #   "splash"         # Enable splash screen
-  #   "plymouth.ignore-serial-consoles"
-  # ];
-  
-  # # Add artificial delay to see Plymouth (remove after testing)
-  # boot.initrd.systemd.services.plymouth-delay = {
-  #   description = "Delay to see Plymouth";
-  #   wantedBy = [ "initrd.target" ];
-  #   before = [ "initrd.target" ];
-  #   serviceConfig = {
-  #     Type = "oneshot";
-  #     ExecStart = "${pkgs.coreutils}/bin/sleep 10";
-  #   };
-  # };
+  boot = {
+    plymouth = {
+      enable = true;
+      theme = "breeze";
+    };
+    consoleLogLevel = 3;
+    initrd.verbose = false;
+    kernelParams = [
+      "quiet"
+      "splash"
+      "boot.shell_on_fail"
+      "udev.log_priority=3"
+      "rd.systemd.show_status=auto"
+    ];
+  };
 
   # # Create custom theme
   # boot.plymouth.themePackages = [
