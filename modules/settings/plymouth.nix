9894d6028157a9b048623ee72d4e31740ffdbdc2
JakeClark <jakeclark38b@gmail.com>
2025-09-03 14:06:29 +0700

    Add custom Plymouth theme with image display and update configuration

--- modules/settings/plymouth.nix
+++ modules/settings/plymouth.nix
@@ -1,11 +1,28 @@
 { config, pkgs, ... }:
-
+let
+  myPlymouthTheme = pkgs.stdenv.mkDerivation {
+    name = "my-theme";
+    src = ./plymouth-themes/my-theme;
+    
+    installPhase = ''
+      mkdir -p $out/share/plymouth/themes/my-theme
+      cp -r * $out/share/plymouth/themes/my-theme/
+      
+      # Replace @out@ placeholder with actual output path
+      substituteInPlace $out/share/plymouth/themes/my-theme/my-theme.plymouth \
+        --replace "@out@" "$out"
+    '';
+  };
+in
 {
   boot = {
     plymouth = {
       enable = true;
-      theme = "spinner";
-      themePackages = [ pkgs.plymouth ];
+      theme = "my-theme";
+      themePackages = [ 
+        pkgs.plymouth 
+        myPlymouthTheme
+      ];
     };
     consoleLogLevel = 3;
     initrd.verbose = false;
